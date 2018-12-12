4b5912d279a6600ffc492e68b7fd25609d7bd8b0
Lukas Zapletal <lzap+git@redhat.com>
2018-12-12 10:51:40 +0100

    Rubocop: Metrics/AbcSize and parens

--- lib/smart_proxy_dhcp_infoblox/plugin_configuration.rb
+++ lib/smart_proxy_dhcp_infoblox/plugin_configuration.rb
@@ -11,31 +11,31 @@ module Proxy::DHCP::Infoblox
     end
 
     def load_dependency_injection_wirings(c, settings)
-      c.dependency :connection, (lambda do
-                                  ::Infoblox.wapi_version = '2.0'
-                                  ::Infoblox::Connection.new(:username => settings[:username], :password => settings[:password],
-                                                             :host => settings[:server], :ssl_opts => { :verify => true },
-                                                             :logger => ::Proxy::LogBuffer::Decorator.instance)
-                                end)
+      c.dependency :connection, (lambda {
+        ::Infoblox.wapi_version = '2.0'
+        ::Infoblox::Connection.new(:username => settings[:username], :password => settings[:password],
+                                   :host => settings[:server], :ssl_opts => { :verify => true },
+                                   :logger => ::Proxy::LogBuffer::Decorator.instance)
+      })
 
       c.singleton_dependency :unused_ips, lambda { ::Proxy::DHCP::FreeIps.new(settings[:blacklist_duration_minutes]) }
 
-      c.dependency :host_ipv4_crud, (lambda do
+      c.dependency :host_ipv4_crud, (lambda {
         ::Proxy::DHCP::Infoblox::HostIpv4AddressCRUD.new(c.get_dependency(:connection), settings[:dns_view])
-      end)
-      c.dependency :fixed_address_crud, (lambda do
+      })
+      c.dependency :fixed_address_crud, (lambda {
         ::Proxy::DHCP::Infoblox::FixedAddressCRUD.new(c.get_dependency(:connection), settings[:network_view])
-      end)
+      })
       c.dependency :grid_restart, lambda { ::Proxy::DHCP::Infoblox::GridRestart.new(c.get_dependency(:connection)) }
-      c.dependency :dhcp_provider, (lambda do
-                                      ::Proxy::DHCP::Infoblox::Provider.new(
-                                        c.get_dependency(:connection),
-                                        (settings[:record_type] == 'host') ? c.get_dependency(:host_ipv4_crud) : c.get_dependency(:fixed_address_crud),
-                                        c.get_dependency(:grid_restart),
-                                        c.get_dependency(:unused_ips),
-                                        settings[:subnets],
-                                          settings[:network_view])
-                                    end)
+      c.dependency :dhcp_provider, (lambda {
+        ::Proxy::DHCP::Infoblox::Provider.new(
+          c.get_dependency(:connection),
+          (settings[:record_type] == 'host') ? c.get_dependency(:host_ipv4_crud) : c.get_dependency(:fixed_address_crud),
+          c.get_dependency(:grid_restart),
+          c.get_dependency(:unused_ips),
+          settings[:subnets],
+          settings[:network_view])
+      })
     end
   end
 end
