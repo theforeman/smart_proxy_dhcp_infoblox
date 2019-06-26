e9ae7dfaea3bc537e514efd30ca1d20f41a5b1d4
Lukas Zapletal <lzap+git@redhat.com>
2019-06-26 12:24:24 +0200

    Fixes #27060 - post-restart grid delay

--- lib/smart_proxy_dhcp_infoblox/dhcp_infoblox_plugin.rb
+++ lib/smart_proxy_dhcp_infoblox/dhcp_infoblox_plugin.rb
@@ -2,7 +2,12 @@ module Proxy::DHCP::Infoblox
   class Plugin < ::Proxy::Provider
     plugin :dhcp_infoblox, ::Proxy::DHCP::Infoblox::VERSION
 
-    default_settings :record_type => 'fixedaddress', :dns_view => "default", :network_view => "default", :blacklist_duration_minutes => 30 * 60
+    default_settings :record_type => 'fixedaddress',
+        :dns_view => "default",
+        :network_view => "default",
+        :blacklist_duration_minutes => 30 * 60,
+        :wait_after_restart => 10
+
     validate_presence :username, :password
 
     requires :dhcp, '>= 1.13'
